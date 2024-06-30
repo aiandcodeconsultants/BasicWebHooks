551b3d52f29d5f0882013a495c60ea3569a7dbd4
James Burton <james@code-consultants.co.uk>
2024-06-30 14:15:47 +0100

    Added LoggerInvoker

---
+++ BasicWebHooks.Core/Invokers/LoggerInvoker.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+using Microsoft.Extensions.Logging;
+
+namespace BasicWebHooks.Core.Invokers;
+
+public class LoggerInvoker(ILogger<LoggerInvoker> logger, TimeProvider? timeProvider = null) : IInvoker
+{
+    private const string Message = "LoggerInvoker @ {Now}\r\nInvocation: {InvocationJson}";
+
+    public ValueTask<bool> TryInvoke(WebHookTargetInvocation targetInvocation, out Exception? exception, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var now = (timeProvider ?? TimeProvider.System).GetUtcNow().UtcDateTime;
+            logger.LogInformation(Message, now, JsonSerializer.Serialize(targetInvocation));
+            exception = null;
+            return ValueTask.FromResult(true);
+        } catch (Exception ex)
+        {
+            exception = ex;
+            return ValueTask.FromResult(false);
+        }
+    }
+}
